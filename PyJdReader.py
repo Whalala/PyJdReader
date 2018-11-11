ab1d8f317becd6672b40bf11551b36c4601b305f
Wha Tang <wha@outlook.com>
2018-11-11 13:47:39 +0800

    1.合并所有截图到pdf文件。 2.图片变为灰阶。

--- PyJdReader.py
+++ PyJdReader.py
@@ -1,10 +1,13 @@
-from PIL import Image, ImageGrab
+import os
 
+from PIL import Image, ImageGrab
+from fpdf import FPDF
 from pyJdHelper import PyJdHelper
 from settings import Settings
 
 
-
+def log(msg):
+    print(msg)
 
 
 class PyJdReader:
@@ -29,7 +32,7 @@ class PyJdReader:
         percent = self.helper.getClipboard()
         return float(percent) < 100
 
-    def savePage2Pictures(self, fp, fromStart = False):
+    def savePage2Pictures(self, fp, startIndex = 1, fromStart = False):
         print("hello. I am PyJdReader.")
         #1. Active the JdReader to foregroud
         self.helper.clickMouseLeft(self.settings.TaskBarLocation)
@@ -38,14 +41,43 @@ class PyJdReader:
             self.helper.clickMouseLeft(self.settings.FirstButtonCenter)
 
         # Screen Clipping
-        pageNo = 1
+        pageNo = startIndex
+
+
         while self.hasMore():
             img = self.clipImage(self.rect)
-            fn = fp + '%05d' % pageNo + '.png'
+            fn = fp + '%05d' % pageNo + '.jpg'
             img.save(fn)
             print('Save to ' + fn)
             pageNo += 1
             self.clickNextPage()
+        # The last page
+        img = self.clipImage(self.rect)
+        fn = fp + '%05d' % pageNo + '.jpg'
+        img.save(fn)
+        print('Save to ' + fn)
+        pageNo += 1
 
     def clickNextPage(self):
         self.helper.clickMouseLeft(self.settings.NextButtonCenter)
+
+    def mergePictures2Pdf(self, imagesPath, pdfPath):
+        pdf = FPDF(orientation='P', unit='mm', format='A4')
+        for im in os.listdir(imagesPath):
+            img = Image.open(imagesPath + im)
+            pdf.add_page()
+            pdf.image(imagesPath + im, -0, 0, pdf.w * 0.99, pdf.h * 0.99)  # , img.width, img.height)
+        pdf.output(pdfPath, "F")
+
+    def convert2Bw(self, imagesPath, out_path, gray = False):
+        for im in os.listdir(imagesPath):
+            img = Image.open(imagesPath + im)
+            if gray:
+                img2 = img.convert('L')
+            else:
+                img2 = img.convert('1')
+            if not os.path.exists(out_path):
+                os.mkdir(out_path)
+                log("mkdir " + out_path)
+            img2.save(out_path + im)
+
--- pyJdTempTester.py
+++ pyJdTempTester.py
@@ -1,11 +1,28 @@
 from PyJdReader import PyJdReader
 
+import os
+from PIL import Image, ImageGrab
+
+
 
 def main():
     reader = PyJdReader()
-    fp = "D:\jdReader\\a\\"
-    fp = 'D:\jdReader\借我执拗如少年\\'
-    reader.savePage2Pictures(fp, True)
+    fpr = "D:\jdReader\\"
+    bn = "十万个小肉段"
+    fp = fpr + bn + "\\"
+    # fp = 'D:\jdReader\\aaaa\\'
+    #screenshot
+    # reader.savePage2Pictures(fp, 1, True)
+    #save to pdf file
+    pdfPath = fpr + bn + ".pdf"
+    # reader.mergePictures2Pdf(fp, pdfPath)
+    #convert images to black-white (or gray level) image
+    reader.convert2Bw(fp, fp + "bw\\", False)
+    pdf_path2 = fpr + bn + "bw.pdf"
+    reader.mergePictures2Pdf(fp + "bw\\", pdf_path2)
+
+
+
 
 
 if __name__ == '__main__':
